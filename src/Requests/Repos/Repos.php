526f056426a236ac731b7f4e062bcddfff366c94
jordanpartridge <jordanpartridge@users.noreply.github.com>
2024-11-01 05:09:38 +0000

    Fix styling

--- src/Requests/Repos/Repos.php
+++ src/Requests/Repos/Repos.php
@@ -29,32 +29,31 @@ class Repos extends Request
     protected Method $method = Method::GET;
 
     /**
-     * @param  int|null   $per_page  Items per page (max 100)
-     * @param int|null    $page       Page number
-     * @param string|null $visibility Can be one of: all, public, private
-     * @param string|null $sort       Can be one of: created, updated, pushed, full_name
-     * @param string|null $direction  Can be one of: asc, desc
+     * @param  int|null  $per_page  Items per page (max 100)
+     * @param  int|null  $page  Page number
+     * @param  string|null  $visibility  Can be one of: all, public, private
+     * @param  string|null  $sort  Can be one of: created, updated, pushed, full_name
+     * @param  string|null  $direction  Can be one of: asc, desc
      */
     public function __construct(
-        protected ?int    $per_page = null,
-        protected ?int    $page = null,
+        protected ?int $per_page = null,
+        protected ?int $page = null,
         protected ?string $visibility = null,
         protected ?string $sort = null,
         protected ?string $direction = null,
-    )
-    {
+    ) {
         if ($this->per_page !== null && ($this->per_page < 1 || $this->per_page > 100)) {
             throw new InvalidArgumentException('Per page must be between 1 and 100');
         }
-        if ($this->visibility !== null && !in_array($this->visibility, ['all', 'public', 'private'])) {
+        if ($this->visibility !== null && ! in_array($this->visibility, ['all', 'public', 'private'])) {
             throw new InvalidArgumentException('Visibility must be one of: all, public, private');
         }
 
-        if ($this->sort !== null && !in_array($this->sort, ['created', 'updated', 'pushed', 'full_name'])) {
+        if ($this->sort !== null && ! in_array($this->sort, ['created', 'updated', 'pushed', 'full_name'])) {
             throw new InvalidArgumentException('Sort must be one of: created, updated, pushed, full_name');
         }
 
-        if ($this->direction !== null && !in_array($this->direction, ['asc', 'desc'])) {
+        if ($this->direction !== null && ! in_array($this->direction, ['asc', 'desc'])) {
             throw new InvalidArgumentException('Direction must be one of: asc, desc');
         }
     }
@@ -62,17 +61,17 @@ class Repos extends Request
     protected function defaultQuery(): array
     {
         return array_filter([
-            'per_page'   => $this->per_page,
-            'page'       => $this->page,
+            'per_page' => $this->per_page,
+            'page' => $this->page,
             'visibility' => $this->visibility,
-            'sort'       => $this->sort,
-            'direction'  => $this->direction,
-        ], fn($value) => $value !== null);
+            'sort' => $this->sort,
+            'direction' => $this->direction,
+        ], fn ($value) => $value !== null);
     }
 
     public function createDtoFromResponse(Response $response): mixed
     {
-        return array_map(fn($repo) => new \JordanPartridge\GithubClient\Data\Repo(...$repo), $response->json());
+        return array_map(fn ($repo) => new \JordanPartridge\GithubClient\Data\Repo(...$repo), $response->json());
 
     }
 
