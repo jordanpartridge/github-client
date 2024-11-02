c3b33b4133bebe1061e7e90a7ddf49ff133fd7b0
Jordan Partridge <jordan@partridge.rocks>
2024-11-02 10:24:32 -0700

    feature/commits (#8)
    * add quick arch organization
    
    * commits list working already
    
    * Fix styling
    
    ---------
    
    Co-authored-by: jordanpartridge <jordanpartridge@users.noreply.github.com>

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,20 @@
 <?php
 
-arch('it will not use debugging functions')
-    ->expect(['dd', 'dump', 'ray'])
-    ->each->not->toBeUsed();
+use JordanPartridge\GithubClient\Resources\BaseResource;
+
+describe('General', function () {
+    it('will not use debugging functions', function () {
+        expect(['dd', 'dump', 'ray'])->each->not->toBeUsed();
+    });
+});
+describe('Resources', function () {
+    arch('resources extend the base resource', function () {
+        expect('JordanPartridge\GithubClient\Response')->toExtend(BaseResource::class);
+    });
+});
+
+describe('Requests', function () {
+    arch('requests extend the base resource', function () {
+        expect('JordanPartridge\GithubClient\Requests')->toExtend(\Saloon\Http\Request::class);
+    });
+});
