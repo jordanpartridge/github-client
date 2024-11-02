2197c56e4843e830ed18946e3961a1af3dc4ca46
Jordan <jordan.l.partridge@gmail.com>
2024-11-02 09:51:11 -0700

    add quick arch organization

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,15 @@
 <?php
 
-arch('it will not use debugging functions')
-    ->expect(['dd', 'dump', 'ray'])
-    ->each->not->toBeUsed();
+use JordanPartridge\GithubClient\Resources\BaseResource;
+
+describe('General', function () {
+    it('will not use debugging functions', function () {
+        expect(['dd', 'dump', 'ray'])->each->not->toBeUsed();
+    });
+});
+describe('Resources', function () {
+    arch('resources extend the base resource', function () {
+        expect('JordanPartridge\GithubClient\Resource')->toExtend(BaseResource::class);
+    });
+});
+
