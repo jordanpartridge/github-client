8601207942c261a11a487a41cfe60e1e394a9dba
jordanpartridge <jordanpartridge@users.noreply.github.com>
2024-11-26 04:12:21 +0000

    Fix styling

--- src/Requests/Commits/Get.php
+++ src/Requests/Commits/Get.php
@@ -8,32 +8,23 @@ use Saloon\Http\Request;
 
 class Get extends Request
 {
-    /**
-     * @var Method
-     */
     protected Method $method = Method::GET;
 
-    /**
-     * @param string $commit_sha
-     */
     public function __construct(
         private readonly string $commit_sha,
-    )
-    {
-       $this->validateSHA($commit_sha);
+    ) {
+        $this->validateSHA($commit_sha);
     }
 
     public function resolveEndpoint(): string
     {
-        return '/commits/' . $this->commit_sha;
+        return '/commits/'.$this->commit_sha;
     }
 
     private function validateSHA(string $commit_sha): void
     {
-        if (!preg_match('/^[0-9a-f]{40}$/i', $commit_sha)) {
+        if (! preg_match('/^[0-9a-f]{40}$/i', $commit_sha)) {
             throw new InvalidArgumentException('Invalid commit SHA format');
         }
     }
-
-
 }
