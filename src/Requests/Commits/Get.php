550277835e1c2e6f7861d2fa9198070998b38fc6
Jordan <jordan.l.partridge@gmail.com>
2024-11-25 21:26:41 -0700

    fit get commit to include repo

--- src/Requests/Commits/Get.php
+++ src/Requests/Commits/Get.php
@@ -3,22 +3,35 @@
 namespace JordanPartridge\GithubClient\Requests\Commits;
 
 use InvalidArgumentException;
+use JordanPartridge\GithubClient\Concerns\ValidatesRepoName;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
 
 class Get extends Request
 {
+    use ValidatesRepoName;
+
+    /**
+     * @var Method
+     */
     protected Method $method = Method::GET;
 
+    /**
+     * @param string $repo_name
+     * @param string $commit_sha
+     */
     public function __construct(
+        private readonly string $repo_name,
         private readonly string $commit_sha,
-    ) {
+    )
+    {
+        $this->validateRepoName($this->repo_name);
         $this->validateSHA($commit_sha);
     }
 
     public function resolveEndpoint(): string
     {
-        return '/commits/'.$this->commit_sha;
+        return '/repos/' . $this->repo_name . '/commits/' . $this->commit_sha;
     }
 
     private function validateSHA(string $commit_sha): void
