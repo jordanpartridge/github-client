963414be2e06e5bf864d4e3fc0a7e02c84470ded
jordanpartridge <jordanpartridge@users.noreply.github.com>
2024-12-04 16:08:14 +0000

    Fix styling

--- src/Resources/IssueResource.php
+++ src/Resources/IssueResource.php
@@ -7,15 +7,14 @@ use JordanPartridge\GithubClient\Requests\Issues\CreateIssueRequest;
 use JordanPartridge\GithubClient\Requests\Issues\GetIssueRequest;
 use JordanPartridge\GithubClient\Requests\Issues\ListIssuesRequest;
 use JordanPartridge\GithubClient\Requests\Issues\UpdateIssueRequest;
-use Saloon\Http\Response;
 
 class IssueResource extends BaseResource
 {
     /**
      * List issues for a repository
      *
-     * @param string $repository Repository in format "owner/repo"
-     * @param array $filters Optional filters (state, labels, sort, direction, since)
+     * @param  string  $repository  Repository in format "owner/repo"
+     * @param  array  $filters  Optional filters (state, labels, sort, direction, since)
      * @return array<Issue>
      */
     public function all(string $repository, array $filters = []): array
@@ -31,9 +30,8 @@ class IssueResource extends BaseResource
     /**
      * Get a specific issue
      *
-     * @param string $repository Repository in format "owner/repo"
-     * @param int $number Issue number
-     * @return Issue
+     * @param  string  $repository  Repository in format "owner/repo"
+     * @param  int  $number  Issue number
      */
     public function get(string $repository, int $number): Issue
     {
@@ -45,11 +43,10 @@ class IssueResource extends BaseResource
     /**
      * Create a new issue
      *
-     * @param string $repository Repository in format "owner/repo"
-     * @param string $title Issue title
-     * @param string $body Issue body
-     * @param array $options Additional options (labels, assignees, milestone)
-     * @return Issue
+     * @param  string  $repository  Repository in format "owner/repo"
+     * @param  string  $title  Issue title
+     * @param  string  $body  Issue body
+     * @param  array  $options  Additional options (labels, assignees, milestone)
      */
     public function create(string $repository, string $title, string $body, array $options = []): Issue
     {
@@ -66,10 +63,9 @@ class IssueResource extends BaseResource
     /**
      * Update an existing issue
      *
-     * @param string $repository Repository in format "owner/repo"
-     * @param int $number Issue number
-     * @param array $data Update data (title, body, state, labels, assignees, milestone)
-     * @return Issue
+     * @param  string  $repository  Repository in format "owner/repo"
+     * @param  int  $number  Issue number
+     * @param  array  $data  Update data (title, body, state, labels, assignees, milestone)
      */
     public function update(string $repository, int $number, array $data): Issue
     {
