34a4a412e5d27d79b3075aaf0c80bc0091e1f6d8
Jordan Partridge <jordan.l.partridge@gmail.com>
2024-12-11 22:05:47 -0700

    Feature: Comprehensive DTO Library
    - Implemented new DTO structure for GitHub resources
    - Added DataTransformableInterface for consistent API response handling
    - Created shared utility DTOs
    - Improved type safety and validation
    - Added README for documentation
    
    Closes #TODO (add actual issue number when created)

---
+++ src/DataTransferObjects/GithubResourceDtos.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace JordanPartridge\GithubClient\DataTransferObjects;
+
+use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Attributes\MapName;
+use Spatie\LaravelData\Mappers\SnakeCaseMapper;
+use Spatie\LaravelData\Attributes\Validation;
+use Spatie\LaravelData\Optional;
+
+// (Previous full content of the DTOs)
