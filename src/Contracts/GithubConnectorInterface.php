6116e6f0c68900a3b9df40c07fbfe7073c9ea4a9
Jordan Partridge <jordan@partridge.rocks>
2025-05-13 13:11:51 -0700

    Fix GithubConnectorInterface to include HTTP methods

--- src/Contracts/GithubConnectorInterface.php
+++ src/Contracts/GithubConnectorInterface.php
@@ -6,14 +6,22 @@ use JordanPartridge\GithubClient\Resources\CommitResource;
 use JordanPartridge\GithubClient\Resources\FileResource;
 use JordanPartridge\GithubClient\Resources\PullRequestResource;
 use JordanPartridge\GithubClient\Resources\RepoResource;
+use Saloon\Http\Request;
+use Saloon\Http\Response;
 
 interface GithubConnectorInterface
 {
+    // Resource getters
     public function repos(): RepoResource;
-
     public function commits(): CommitResource;
-
     public function files(): FileResource;
-
     public function pullRequests(): PullRequestResource;
+    
+    // HTTP methods
+    public function send(Request $request): Response;
+    public function get(string $url, array $parameters = []): array;
+    public function post(string $url, array $parameters = []): array;
+    public function patch(string $url, array $parameters = []): array;
+    public function put(string $url, array $parameters = []): array;
+    public function delete(string $url, array $parameters = []): array;
 }
