d5f658ed4cb6c4a2eead774f2235d47fe90c17ea
Jordan Partridge <jordan@partridge.rocks>
2025-05-15 21:30:55 -0700

    Address CodeRabbit feedback for Laravel 12 PR
    - Add nunomaduro/collision dependency constraint to fix conflict with Pest
    - Update PHPStan workflow to run on PHP 8.2, 8.3, and 8.4
    - Fix testbench version for Laravel 12 in test workflow
    - Add PHPDoc comments for HTTP methods in GithubConnectorInterface

--- src/Contracts/GithubConnectorInterface.php
+++ src/Contracts/GithubConnectorInterface.php
@@ -21,15 +21,56 @@ interface GithubConnectorInterface
     public function pullRequests(): PullRequestResource;
 
     // HTTP methods
+    /**
+     * Send a raw Saloon request to the GitHub API
+     *
+     * @param Request $request The Saloon request to send
+     * @return Response The Saloon response
+     */
     public function send(Request $request): Response;
 
+    /**
+     * Make a GET request to the GitHub API
+     *
+     * @param string $url The endpoint URL
+     * @param array $parameters Query parameters
+     * @return array Response data as array
+     */
     public function get(string $url, array $parameters = []): array;
 
+    /**
+     * Make a POST request to the GitHub API
+     *
+     * @param string $url The endpoint URL
+     * @param array $parameters Request body data
+     * @return array Response data as array
+     */
     public function post(string $url, array $parameters = []): array;
 
+    /**
+     * Make a PATCH request to the GitHub API
+     *
+     * @param string $url The endpoint URL
+     * @param array $parameters Request body data
+     * @return array Response data as array
+     */
     public function patch(string $url, array $parameters = []): array;
 
+    /**
+     * Make a PUT request to the GitHub API
+     *
+     * @param string $url The endpoint URL
+     * @param array $parameters Request body data
+     * @return array Response data as array
+     */
     public function put(string $url, array $parameters = []): array;
 
+    /**
+     * Make a DELETE request to the GitHub API
+     *
+     * @param string $url The endpoint URL
+     * @param array $parameters Query parameters
+     * @return array Response data as array
+     */
     public function delete(string $url, array $parameters = []): array;
 }
